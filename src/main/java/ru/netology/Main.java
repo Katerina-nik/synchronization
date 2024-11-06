0ada141aa298cc9404070787ec7a5bfb2e29af3a
Katerina-nik <eka-pukhti@yandex.ru>
2024-11-06 21:16:45 +0300

    task2

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -2,7 +2,6 @@ package ru.netology;
 
 import java.util.*;
 
-
 public class Main {
     public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
     public static final int LENGTH_ROUTE = 100;
@@ -10,8 +9,27 @@ public class Main {
     public static final String LETTERS = "RLRFR";
 
     public static void main(String[] args) throws InterruptedException {
+        Thread textThread2 = new Thread(() -> {
+
+            synchronized (sizeToFreq) {
+                while (!Thread.interrupted()) {
+                    try {
+                        sizeToFreq.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    int maxCountInMoment = sizeToFreq.entrySet().stream().max(Comparator.comparingInt(Map.Entry::getValue)).get().getKey();
+                    System.out.printf("Самое частое количество повторений: %d встретился %d раз \n", maxCountInMoment, sizeToFreq.get(maxCountInMoment));
+                }
+            }
+
+        });
+        textThread2.start();
+
 
         List<Thread> threads = new ArrayList<>();
+
+
         for (int i = 0; i < THREADS; i++) {
             Thread textThread = new Thread(() -> {
                 String texts = generateRoute(LETTERS, LENGTH_ROUTE);
@@ -24,6 +42,7 @@ public class Main {
                     } else {
                         sizeToFreq.put(amountR, 1);
                     }
+                    sizeToFreq.notify();
                 }
             });
 
@@ -32,15 +51,15 @@ public class Main {
         for (Thread thread : threads) {
             thread.join();
         }
+        textThread2.interrupt();
 
-        int maxCount = sizeToFreq.entrySet().stream().max(Comparator.comparingInt(Map.Entry::getValue)).get().getKey();
-        System.out.printf("Самое частое количество повторений: %d встретился %d раз \n", maxCount, sizeToFreq.get(maxCount));
 
         System.out.println("Другие размеры:");
         for (Integer key : sizeToFreq.keySet()) {
             int value = sizeToFreq.get(key);
             System.out.println("- " + key + " (" + value + " раз)");
         }
+
     }
 
     public static String generateRoute(String letters, int length) {
